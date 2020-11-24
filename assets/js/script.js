4b048513047b1e5f0c89be9253ee1927c6c9abb6
Jarrett Devose <jarrett.dev1997@gmail.com>
2020-11-23 19:42:18 -0500

    having issues displaying dynamic data onto webpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,13 @@ var citySearchTerm = document.querySelector("#city-search-term");
 
 var getUserCity = function(city) {
     //format url to accept city name input 
-    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=2e409b91144b4a52d9ccf84785e21a6d";
+    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=2e409b91144b4a52d9ccf84785e21a6d";
 
     //make request to url
     fetch(apiUrl).then(function(response){
     response.json().then(function(data){
         displayWeather(data, city);
-    });
+    })
     
   });
 };
@@ -44,7 +44,8 @@ var displayWeather = function(weather, cityName) {
 
     // clears old content
     weatherContainerEl.textContent = "";
-    citySearchTerm.textContent = cityName
+    citySearchTerm.textContent = cityName;
+
 
 
 };
